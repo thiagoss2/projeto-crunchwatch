4be58a4fbfa320481e505fecc1829efbe005b54b
thiagoss2 <thiagojklb@gmail.com>
2022-11-02 11:17:04 -0300

    confirmo

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// CRANDO A AÇÃO DE CLIQUE
+
+const btnHamburger = document.querySelector('[js-btn-hamburger]');
+const menuLateralFixo = document.querySelector('[js-menu-lateral-fixo]');
+const menuLateralFixoBtnX = document.querySelector('[js-menu-lateral-fixo-btn-x]');
+
+
+
+const menuLateralFixoOculto = function() {
+    menuLateralFixo.classList.toggle("ativo");
+}
+
+
+const menuLateralFixoAtivo  = function () {
+    menuLateralFixo.classList.toggle("ativo");
+}
+
+btnHamburger.addEventListener('click' , menuLateralFixoAtivo);
+menuLateralFixoBtnX.addEventListener('click' , menuLateralFixoOculto );
+
+//  elemento.onClick = funcaoQueSeraExecutada();
+
+
+// BLOCO DROPDOWN EPISODEOS
+const episodeosDescricaoBloco = document.querySelector('[js-episodeos-descricao-media]');
+const  dropdownepisodeosDescricao = document.querySelector('[js-episodeos-descricao-dropdown]');
+
+episodeosDescricaoBloco.addEventListener('click', function () {
+    dropdownepisodeosDescricao.classList.toggle("ativo")
+});
